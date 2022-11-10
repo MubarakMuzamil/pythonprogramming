79f7b0f6326661da0ba8b0b09d0ebe2883c5f770
Mubarak <mubarakm143143@gmail.com>
2022-11-10 17:42:33 +0530

    wishing first file

---
+++ chatbot 2.py
@@ -0,0 +1,18 @@
+print("hello,welcome to the Domino's")
+print("If you place more then two order, you will get 20%OFF")
+print("what do you want to buy\n")
+print("Hit 1 for Pizza")
+print("Hit 2 for Burger")
+print("Hit 3 for Pasta")
+userinput = int(input("enter your choice:"))
+if userinput == 1:
+    print("your pizza will be delivered to your address")
+if userinput == 2:
+    print("your burger will be delivered to your address ")
+if userinput == 3:
+    print("your Pasta will be delivered to your address")
+else:
+    print("invalid detail")
+    print("please hit the valid number")
+
+print("thankyou for using our service")
